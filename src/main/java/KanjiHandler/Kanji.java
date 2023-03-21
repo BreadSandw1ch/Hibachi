04f53e9f48ee265d882061f8e53043738de663b0
Gavin Que <gsq7332@rit.edu>
2023-03-21 16:42:58 -0400

    Added Kanji record

---
+++ src/main/java/KanjiHandler/Kanji.java
@@ -0,0 +1,31 @@
+package KanjiHandler;
+
+import java.util.HashSet;
+
+public record Kanji(String kanji, HashSet<String> readings, HashSet<String> meanings) {
+
+    public void addReading(String reading) {
+        readings.add(reading);
+    }
+
+    public void removeReading(String reading) {
+        readings.remove(reading);
+    }
+
+    public boolean hasReading(String reading) {
+        return readings.contains(reading);
+    }
+
+    public void addMeaning(String meaning) {
+        meanings.add(meaning);
+    }
+
+    public void removeMeaning(String meaning) {
+        meanings.remove(meaning);
+    }
+
+    public boolean hasMeaning(String meaning) {
+        return meanings.contains(meaning);
+    }
+
+}
